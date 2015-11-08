3a9294e62a0915cc928392dcea51489efe7cab6a
Kacper Grzeszczyk <kacgrzes@gmail.com>
2015-11-09 00:02:34 +0100

    Added first vue component, created transition for map

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,21 +1,42 @@
+//Zamockowane dane
+var mockedData = {
+    people: [
+        {
+            id: 0,
+            firstName: 'Tomasz',
+            lastName: 'Ciunel'
+        },
+        {
+            id: 1,
+            firstName: 'Kacper',
+            lastName: 'Grzeszczyk'
+        },{
+            id: 2,
+            firstName: 'Jakub',
+            lastName: 'Baczyński'
+        }
+    ]
+};
+
+//Nowy komponent "mapa"
+var map = Vue.extend({
+    template: '<div>Test komponentów - tutaj będzie mapa</div>'
+});
+
+//Rejestracja komponentów
+Vue.component('google-map', map);
+
 var people = new Vue({
     el: '#table',
     data: {
-        people: [
-            {
-                id: 0,
-                firstName: 'Tomasz',
-                lastName: 'Ciunel'
-            },
-            {
-                id: 1,
-                firstName: 'Kacper',
-                lastName: 'Grzeszczyk'
-            },{
-                id: 2,
-                firstName: 'Jakub',
-                lastName: 'Baczyński'
-            }
-        ]
+        people: mockedData.people
+    }
+});
+
+//Root apki
+var app = new Vue({
+    el: '#app',
+    data: {
+        show: true
     }
 });
