08233218df210285c2dbb222e4f5b4af2aa907c5
Kacper Grzeszczyk <kacgrzes@gmail.com>
2015-11-21 17:24:01 +0100

    change configuration not to load react every time new bundle is created

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,31 @@
 var path = require('path');
+var node_modules = path.resolve(__dirname, 'node_modules');
+var pathToReact = path.resolve(node_modules, 'react/dist/react.min.js');
 
-
-module.exports = {
+var config = {
     entry: [
         'webpack/hot/dev-server',
         'webpack-dev-server/client?http://localhost:8080',    
         path.resolve(__dirname, 'app/main.js')
     ],
+    resolve: {
+        alias: {
+            'react': pathToReact
+        }
+    },
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'bundle.js',
     },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                loader: 'jsx'
+            }
+        ],
+        noParse: [pathToReact]
+    }
 };
+
+module.exports = config;
