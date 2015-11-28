857caaa102f564de79f48492fa0c77d3ed2c9f95
Kacper Grzeszczyk <kacgrzes@gmail.com>
2015-11-28 16:40:47 +0100

    fix webpack for easier development

--- webpack.config.js
+++ webpack.config.js
@@ -5,28 +5,46 @@ var path = require('path');
 var node_modules = path.resolve(__dirname, 'node_modules');
 var buildPath = path.resolve(__dirname, 'build');
 
+// if build is false then You are in development mode (use "npm run dev");
+var build = false;
+var cfg = {}
+
+// configuration for entry 
+cfg.entry = build ? {
+    app: [ 
+        path.resolve(__dirname, 'app/App.jsx')
+    ],
+    vendors: ['react']
+} : 
+{
+    app: [
+        'webpack/hot/dev-server',
+        'webpack-dev-server/client?http://localhost:8080',
+        path.resolve(__dirname, 'app/App.jsx')
+    ]
+};
+
+// configuration for plugins
+cfg.plugins = build ? [
+        new webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.js'),
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: 'build/index.html'
+        })
+    ] : [];
+
+
 //WEBPACK CONFIG
 var config = {
     addVendor: function (name, path) {
         this.resolve.alias[name] = path;
         this.module.noParse.push(new RegExp('^' + name + '$'));
     },
-    entry: {
-        app: [ 
-            path.resolve(__dirname, 'app/App.jsx')
-        ]
-        // vendors: ['react']
+    entry: cfg.entry,
+    resolve: {
+        alias: {}
     },
-    // resolve: {
-    //     alias: {}
-    // },
-    plugins: [
-        // new webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.js'),
-        new HtmlWebpackPlugin({
-            inject: true,
-            template: 'build/index.html'
-        })
-    ],
+    plugins: cfg.plugins,
     output: {
         path: buildPath,
         filename: 'bundle.js'
@@ -62,17 +80,19 @@ var config = {
     }
 };
 
-//PATHS TO MODULES
-// var pathToReact = path.resolve(node_modules, 'react/dist/react-with-addons.js');
-// var pathToReactDOM = path.resolve(node_modules, 'react-dom/dist/react-dom.js');
-// var pathToInject = path.resolve(node_modules, 'react-tap-event-plugin/src/injectTapEventPlugin.js');
-// var pathToReactMDL = path.resolve(node_modules, 'react-mdl/index.js');
-// var mdl = path.resolve(node_modules, 'material-ui/lib/index.js');
+if(build) {
+   // PATHS TO MODULES
+    var pathToReact = path.resolve(node_modules, 'react/dist/react-with-addons.js');
+    var pathToReactDOM = path.resolve(node_modules, 'react-dom/dist/react-dom.js');
+    var pathToInject = path.resolve(node_modules, 'react-tap-event-plugin/src/injectTapEventPlugin.js');
+    var pathToReactMDL = path.resolve(node_modules, 'react-mdl/index.js');
+    var mdl = path.resolve(node_modules, 'material-ui/lib/index.js');
 
-// config.addVendor('react', pathToReact);
-// config.addVendor('react-dom', pathToReactDOM);
-// config.addVendor('react-mdl', pathToReactMDL);
-// config.addVendor('react-tap-event-plugin', pathToInject);
-// config.addVendor('material-ui', mdl);
+    config.addVendor('react', pathToReact);
+    config.addVendor('react-dom', pathToReactDOM);
+    config.addVendor('react-mdl', pathToReactMDL);
+    config.addVendor('react-tap-event-plugin', pathToInject);
+    config.addVendor('material-ui', mdl); 
+}
 
 module.exports = config;
